102f3a4fabb76d2d0d4004f6b5ca7563a6048efc
madflytom <madflytom@gmail.com>
2022-12-26 06:10:14 -0500

    extracting details view

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -7,12 +7,75 @@
 
 import SwiftUI
 
+let nightlyTasks = [
+    "Check all windows",
+    "Check all doors",
+    "Check that the safe is locked",
+    "Check the mailbox",
+    "Inspect security cameras",
+    "Clear ice from sidewalks",
+    "Document \"strange and unusual\" occurences"
+]
+
+let weeklyTasks = [
+    "Check inside all vacant rooms",
+    "Walk the perimeter of property"
+]
+
+let monthlyTasks = [
+    "Test security alarm",
+    "Test motion detectors",
+    "Test smoke alarms"
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section(header: TaskSectionHeader(symbolSystemName: "moon.stars", headerText: "Nightly Tasks")) {
+                    ForEach(nightlyTasks, id: \.self, content: {
+                        taskName in
+                        NavigationLink(taskName,
+                                       destination: DetailsView(task: taskName))
+                    })
+                }
+                
+                Section(header: TaskSectionHeader(symbolSystemName: "sunset", headerText: "Weekly Tasks")) {
+                    ForEach(weeklyTasks, id: \.self, content: {
+                        taskName in
+                        NavigationLink(taskName,
+                                       destination: DetailsView(task: taskName))
+                    })
+                }
+                Section(header: TaskSectionHeader(symbolSystemName: "calendar", headerText: "Monthly Tasks")) {
+                    ForEach(monthlyTasks, id: \.self, content: {
+                        taskName in
+                        NavigationLink(taskName,
+                                       destination: DetailsView(task: taskName))
+                    })
+                }
+            }.listStyle(GroupedListStyle())
+            .navigationTitle("Home")
+        }
     }
 }
+
+
+
+struct TaskSectionHeader: View {
+    let symbolSystemName: String
+    let headerText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: symbolSystemName)
+            Text(headerText)
+        }
+        .font(.title3)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
